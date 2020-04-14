65a6e6b0485522b86c4840f5b54dfec2122ac69c
musasabi333 <musasabi333@gmail.com>
2020-04-14 22:10:59 +0200

    c6 - Fixed typo

--- angular-matf/src/app/bookstores/books.service.ts
+++ angular-matf/src/app/bookstores/books.service.ts
@@ -10,7 +10,7 @@ export class BooksService {
     new Book('American Gods', 'Neil Gaiman', 9.99),
     new Book('The Way of Kings', 'Brandon Sanderson', 8.99),
     new Book('Ringworld', 'Larry Niven', 4.89),
-    new Book('Norvegian Wood', 'Haruki Murakami', 7.99),
+    new Book('Norwegian Wood', 'Haruki Murakami', 7.99),
     new Book('The Hitchhiker\'s Guide to the Galaxy', 'Douglas Adams', 6.89),
     new Book('Autobiografija', 'Branislav Nusic', 6.99),
     new Book('Harry Potter and the Chamber of Secrets', 'J. K. Rowling', 7.29),
